4028cda3ae2af18c3174238b43fe6198bc47d9eb
itzhake <itzikil@icloud.com>
2022-12-14 17:00:21 +0200

    1.5

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/homework-pixel-prefect/',
+  base: '/homework-pixel-perfect/',
   plugins: [vue()],
   resolve: {
     alias: {
