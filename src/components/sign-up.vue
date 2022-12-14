b5e83d96f9f8b482bfa62eda99f9c047a6faf17b
itzhake <itzikil@icloud.com>
2022-12-14 15:28:11 +0200

    1.3

--- src/components/sign-up.vue
+++ src/components/sign-up.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class="sign-up">
+    <section class="sign-up flex column">
         <h2>Get early access today</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium atque quidem soluta minus aliquid est </p>
-        <div class="sign-up input">
+        <div class="sign-up-input flex">
             <input type="text">
             <button class="btn">Get started for free</button>
         </div>
